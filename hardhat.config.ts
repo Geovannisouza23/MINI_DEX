a15500478b7948919fb11cf65d6fd8904258ca5d
Geovanni Souza <geovanni.dev23@hotmail.com>
2026-02-16 00:55:44 -0300

    chore(config): update compose and tooling

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,12 @@
 import hardhatEthers from "@nomicfoundation/hardhat-ethers";
+import hardhatMocha from "@nomicfoundation/hardhat-mocha";
 import { configVariable, defineConfig } from "hardhat/config";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
-  plugins: [hardhatEthers],
+  plugins: [hardhatEthers, hardhatMocha],
   solidity: {
     profiles: {
       default: {
